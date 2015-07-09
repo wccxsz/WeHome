0ac043c4ae6c537b6d73163e529c9ef80c234dbe
whl <wccxsz@126.com>
2015-07-09 23:02:56 +0800

    ~

---
+++ WeHome.Web/Controllers/SingleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeHome.Web.Controllers
+{
+    [Authorize]
+    public class SingleController : Controller
+    {
+        // GET: Single
+        public ActionResult Index()
+        {
+            ViewBag.Account = HttpContext.User.Identity.Name;
+            return View();
+        }
+
+        [Route("user/videos")]
+        public ActionResult Video()
+        {
+            return View();
+        }
+    }
+}
---
+++ WeHome.Web/LocalClass/NonOrderingBundleOrderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace WeHome.Web.LocalClass
+{
+    public class NonOrderingBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+}
